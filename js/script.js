f6800783168a0c862cf9f9b6553a75697d77634d
Dayany Espindola <dayanyrec@gmail.com>
2015-02-26 16:46:57 -0300

    draw static clock

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ window.onload = function () {
 		ctx;
 
 	ctx = getCtx(draw);
+	if (ctx === null) return;
 
 	setSize(draw);
+
+	drawClock(ctx, draw.width / 2, draw.height / 2);
 }
 
 function getCtx (canvas) {
@@ -19,4 +22,27 @@ function getCtx (canvas) {
 function setSize (canvas) {
 	canvas.width = document.body.clientWidth;
 	canvas.height = document.body.clientHeight;
+}
+
+function drawClock (ctx, x, y) {
+	var r = 0.7 * y;
+
+	ctx.beginPath();
+	// draw outer circle
+	ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+
+	// draw inner circle
+	ctx.moveTo(x + r - 5, y);
+	ctx.arc(x, y, r - 5, 0, 2 * Math.PI, false);
+
+	// draw minute hand
+	ctx.moveTo(x, y);
+	ctx.lineTo(x, y - r + 15);
+
+	// draw hour hand
+	ctx.moveTo(x, y);
+	ctx.lineTo(x - r + 35, y);
+
+	// stroke the path
+	ctx.stroke();
 }
