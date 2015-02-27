2e2d1182472e345c8dd039bc104e10b9ac3989a4
Dayany Espindola <dayanyrec@gmail.com>
2015-02-27 17:30:43 -0300

    Animate hands

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ window.onload = function () {
 	setSize(draw);
 
 	drawClock(ctx, draw.width / 2, draw.height / 2);
+
+	setInterval(function () {
+		ctx.clearRect(0, 0, draw.width, draw.height);
+		drawClock(ctx, draw.width / 2, draw.height / 2);
+	}, 1000);
+
 }
 
 function getCtx (canvas) {
@@ -26,7 +32,10 @@ function setSize (canvas) {
 
 function drawClock (ctx, x, y) {
 	var r = 0.7 * y,
-		numbersAngle = 2 * Math.PI / 12;
+		numbersAngle = 2 * Math.PI / 12,
+		hourAngle = numbersAngle,
+		minAngle = 2 * Math.PI / 60,
+		date = new Date();
 
 	ctx.beginPath();
 	// draw outer circle
@@ -36,16 +45,31 @@ function drawClock (ctx, x, y) {
 	ctx.moveTo(x + r - 5, y);
 	ctx.arc(x, y, r - 5, 0, 2 * Math.PI, false);
 
+	ctx.save();
+
 	// translate to center
 	ctx.translate(x, y);
 
-	// draw minute hand
+	// draw seconds hand
+	ctx.save();
+	ctx.rotate(minAngle * date.getSeconds());
 	ctx.moveTo(0, 0);
 	ctx.lineTo(0, - r + 15);
+	ctx.restore();
+
+	// draw minute hand
+	ctx.save();
+	ctx.rotate(minAngle * date.getMinutes());
+	ctx.moveTo(0, 0);
+	ctx.lineTo(0, - r + 15);
+	ctx.restore();
 
 	// draw hour hand
+	ctx.save();
+	ctx.rotate(hourAngle * date.getHours());
 	ctx.moveTo(0, 0);
-	ctx.lineTo(- r + 45, 0);
+	ctx.lineTo(0, - r + 65);
+	ctx.restore();
 
 	// stroke the path
 	ctx.stroke();
@@ -61,5 +85,7 @@ function drawClock (ctx, x, y) {
 		ctx.fillText(i, 0, - r + 20);
 		ctx.rotate(numbersAngle);
 	}
+	ctx.restore();
+
 	ctx.restore();
 }
